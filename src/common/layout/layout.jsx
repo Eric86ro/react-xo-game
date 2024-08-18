2bf2088c960616935ee051f48f5bd1d42973593b
Eric86ro <eric86ro@yahoo.com>
2024-08-18 12:16:06 +0300

    added project

---
+++ src/common/layout/layout.jsx
@@ -0,0 +1,12 @@
+import styles from "./layout.module.css";
+
+const Layout = (props) => {
+    return (
+      <main className={styles["container"]}>
+        <div className={styles["alignment"]}>{props.children}</div>
+        
+      </main>
+    );
+};
+
+export default Layout;
